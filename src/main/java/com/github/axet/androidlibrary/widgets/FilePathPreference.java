e0a2016299fedfbfe41868ed639c948de47ac914
Alexey Kuznetsov <axet@me.com>
2017-03-05 00:18:02 +0300

    dialog type

--- src/main/java/com/github/axet/androidlibrary/widgets/FilePathPreference.java
+++ src/main/java/com/github/axet/androidlibrary/widgets/FilePathPreference.java
@@ -32,7 +32,7 @@ public class FilePathPreference extends EditTextPreference {
     }
 
     public void showDialog(Activity activity) {
-        f = new OpenFileDialog(getContext(), OpenFileDialog.DIALOG_TYPE.BOOTH);
+        f = new OpenFileDialog(getContext(), OpenFileDialog.DIALOG_TYPE.FILE_DIALOG);
 
         String path = getText();
 
