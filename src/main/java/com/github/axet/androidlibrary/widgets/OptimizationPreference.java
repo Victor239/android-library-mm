27ec7369a9ba8400c7db4a6377a4d9a5e5d3ad12
Alexey Kuznetsov <axet@me.com>
2017-04-07 20:11:06 +0300

    add optimization option

---
+++ src/main/java/com/github/axet/androidlibrary/widgets/OptimizationPreference.java
@@ -0,0 +1,71 @@
+package com.github.axet.androidlibrary.widgets;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.SwitchPreferenceCompat;
+import android.util.AttributeSet;
+
+/**
+ * Add users permission to app manifest:
+ *
+ *     <uses-permission android:name="android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS" />
+ */
+public class OptimizationPreference extends SwitchPreferenceCompat {
+
+    @TargetApi(21)
+    public OptimizationPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        create();
+    }
+
+    @TargetApi(21)
+    public OptimizationPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        create();
+    }
+
+    public OptimizationPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        create();
+    }
+
+    public OptimizationPreference(Context context) {
+        super(context);
+        create();
+    }
+
+    void create() {
+    }
+
+    public void onResume() {
+        final PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
+        final String n = getContext().getPackageName();
+        if (Build.VERSION.SDK_INT < 23) {
+            setVisible(false);
+        } else {
+            setChecked(pm.isIgnoringBatteryOptimizations(n));
+            setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                @TargetApi(23)
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    if (pm.isIgnoringBatteryOptimizations(n)) {
+                        Intent intent = new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                        getContext().startActivity(intent);
+                    } else {
+                        Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                        intent.setData(Uri.parse("package:" + n));
+                        getContext().startActivity(intent);
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+
+}
