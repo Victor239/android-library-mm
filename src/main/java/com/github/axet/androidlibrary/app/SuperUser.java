0659d0bb4de2f9ff25c38fb5739440e0ebcb5b71
Alexey Kuznetsov <axet@me.com>
2017-12-27 03:20:12 +0300

    cleanups

--- src/main/java/com/github/axet/androidlibrary/app/SuperUser.java
+++ src/main/java/com/github/axet/androidlibrary/app/SuperUser.java
@@ -12,24 +12,47 @@ public class SuperUser {
     public static String TAG = SuperUser.class.getSimpleName();
 
     public static final String SYSTEM = "/system";
-    public static final String ETC = SYSTEM + "/etc";
+    public static final String ETC = "/etc";
+    public static final String USR = "/usr";
 
-    public static final String BIN_SU = find(SYSTEM + "/xbin/su", "/sbin/su");
-    public static final String BIN_TRUE = find("/usr/bin/true", "/bin/true");
-    public static final String BIN_REBOOT = find(SYSTEM + "/bin/reboot", "/sbin/reboot", "/bin/reboot");
+    public static final String BIN_SU = path("su");
+    public static final String BIN_TRUE = path("true");
+    public static final String BIN_REBOOT = path("reboot");
+    public static final String BIN_MOUNT = path("mount");
+    public static final String BIN_CAT = path("cat");
+    public static final String BIN_TOUCH = path("touch");
+    public static final String BIN_RM = path("rm");
+    public static final String BIN_MKDIR = path("mkdir");
+    public static final String BIN_CHMOD = path("chmod");
+    public static final String BIN_CHOWN = path("chown");
+    public static final String BIN_MV = path("mv");
+    public static final String BIN_CP = path("cp");
+    public static final String BIN_KILL = path("kill");
 
-    public static final String SUCAT = "cat << EOF > {0}\n{1}\nEOF";
-    public static final String MOUNT = "mount {0}";
+    public static final String SUCAT = BIN_CAT + " << EOF > {0}\n{1}\nEOF";
+    public static final String MOUNT = BIN_MOUNT + " {0}";
     public static final String REMOUNT_SYSTEM = MessageFormat.format(MOUNT, "-o remount,rw " + SYSTEM);
-    public static final String MKDIRS = "mkdir -p {0}";
-    public static final String TOUCH = "touch -a {0}";
-    public static final String DELETE = "rm -rf {0}";
-    public static final String CHMOD = "chmod {0} {1}";
-    public static final String CHOWN = "chown {0} {1}";
-    public static final String MV = "mv {0} {1} || ( cp {0} {1} && rm {0} )";
+    public static final String MKDIRS = BIN_MKDIR + " -p {0}";
+    public static final String TOUCH = BIN_TOUCH + " -a {0}";
+    public static final String DELETE = BIN_RM + " -rf {0}";
+    public static final String CHMOD = BIN_CHMOD + " {0} {1}";
+    public static final String CHOWN = BIN_CHOWN + "{0} {1}";
+    public static final String MV = BIN_MV + " {0} {1} || ( " + BIN_CP + " {0} {1} && " + BIN_RM + " {0} )";
     public static final String EXIT = "exit";
 
-    public static final String KILL = " || kill -9 $$"; // some su does not return error codes in scripts, kill it
+    public static final String KILL = " || " + BIN_KILL + " -9 $$"; // some su does not return error codes in scripts, kill it
+
+    public static String path(String cmd) {
+        for (String s : new String[]{SYSTEM + "/xbin", SYSTEM + "/sbin", SYSTEM + "/bin",
+                SYSTEM + USR + "/sbin", SYSTEM + USR + "/bin",
+                USR + "/sbin", USR + "/bin",
+                "/sbin", "/bin"}) {
+            String f = find(s + "/" + cmd);
+            if (f != null)
+                return f;
+        }
+        return cmd;
+    }
 
     public static String find(String... args) {
         for (String s : args) {
