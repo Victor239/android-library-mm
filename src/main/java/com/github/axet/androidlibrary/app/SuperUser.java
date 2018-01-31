a21749fd734ab7a7e8f524075efb49df485565a1
Alexey Kuznetsov <axet@me.com>
2018-01-31 12:43:57 +0300

    cleanups

--- src/main/java/com/github/axet/androidlibrary/app/SuperUser.java
+++ src/main/java/com/github/axet/androidlibrary/app/SuperUser.java
@@ -176,45 +176,6 @@ public class SuperUser {
         return null;
     }
 
-    // https://stackoverflow.com/questions/28734455/java-converting-file-pattern-to-regular-expression-pattern
-    public static String wildcard(String wildcard) {
-        StringBuilder s = new StringBuilder(wildcard.length());
-        s.append('^');
-        for (int i = 0, is = wildcard.length(); i < is; i++) {
-            char c = wildcard.charAt(i);
-            switch (c) {
-                case '*':
-                    s.append(".*");
-                    break;
-                case '?':
-                    s.append(".");
-                    break;
-                case '^': // escape character in cmd.exe
-                    s.append("\\");
-                    break;
-                // escape special regexp-characters
-                case '(':
-                case ')':
-                case '[':
-                case ']':
-                case '$':
-                case '.':
-                case '{':
-                case '}':
-                case '|':
-                case '\\':
-                    s.append("\\");
-                    s.append(c);
-                    break;
-                default:
-                    s.append(c);
-                    break;
-            }
-        }
-        s.append('$');
-        return (s.toString());
-    }
-
     public static String escape(String p) {
         p = p.replaceAll(" ", "\\ "); // ' ' -> '\'
         p = p.replaceAll("\"", "\\\""); // '"' -> '\"'
