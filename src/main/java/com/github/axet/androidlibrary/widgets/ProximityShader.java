3883b97efa81999780ed069c4f38ab07c82ae059
Alexey Kuznetsov <axet@me.com>
2018-05-30 21:29:10 +0300

    fix shader

--- src/main/java/com/github/axet/androidlibrary/widgets/ProximityShader.java
+++ src/main/java/com/github/axet/androidlibrary/widgets/ProximityShader.java
@@ -3,11 +3,9 @@ package com.github.axet.androidlibrary.widgets;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
-import android.graphics.PixelFormat;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.InsetDrawable;
 import android.hardware.Sensor;
@@ -15,10 +13,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.view.WindowCallbackWrapper;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -26,16 +20,10 @@ import android.view.WindowManager;
 import android.widget.PopupWindow;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
-public class ProximitySensor implements SensorEventListener {
-    SensorManager sm;
+public class ProximityShader implements SensorEventListener {
     Dialog d;
+    Sensor proximity;
     Context context;
 
     public void clearFlags(WindowManager.LayoutParams attrs, int flags) {
@@ -61,9 +49,28 @@ public class ProximitySensor implements SensorEventListener {
         }
     }
 
-    public ProximitySensor(Context context) {
+    public static void showSystemUI(Window w) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+
+    public static void hideSystemUI(Window w) {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        if (Build.VERSION.SDK_INT >= 11) {
+            w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    public ProximityShader(Context context) {
         this.context = context;
-        sm = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
     }
 
     public void setContext(Context context) {
@@ -76,19 +83,17 @@ public class ProximitySensor implements SensorEventListener {
     }
 
     public void turnScreenOff() {
-        if (!(context instanceof Activity)) // no window create support
+        if (!(context instanceof Activity) || ((Activity) context).isFinishing()) // no window create support
             return;
         if (d != null) // already hidded
             return;
-
-        closeSystemDialogs();
 
         final View anchor = new View(context);
         anchor.setBackgroundColor(Color.BLACK);
 
         d = new Dialog(context);
         d.setCancelable(false);
-        Window w = d.getWindow();
+        final Window w = d.getWindow();
         d.requestWindowFeature(Window.FEATURE_NO_TITLE);
         d.setContentView(anchor);
         if (Build.VERSION.SDK_INT >= 11) {
@@ -96,7 +101,7 @@ public class ProximitySensor implements SensorEventListener {
                 @Override
                 public void onSystemUiVisibilityChange(int visibility) {
                     closeSystemDialogs();
-                    hideSystemUI(d.getWindow());
+                    hideSystemUI(w);
                 }
             });
         }
@@ -109,39 +114,39 @@ public class ProximitySensor implements SensorEventListener {
         w.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         d.show();
 
+        closeSystemDialogs();
         hideSystemUI(w);
     }
 
     public void turnScreenOn() {
         if (d != null) {
-            showSystemUI(d.getWindow());
             d.dismiss();
             d = null;
         }
     }
 
     public void create() {
+        SensorManager sm = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         if (sm != null) {
-            Sensor proximity = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            proximity = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
             if (proximity != null)
                 sm.registerListener(this, proximity, SensorManager.SENSOR_DELAY_NORMAL);
-            else
-                sm = null;
         }
     }
 
     public void close() {
         turnScreenOn();
 
+        SensorManager sm = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         if (sm != null) {
             sm.unregisterListener(this);
-            sm = null;
+            proximity = null;
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (sm == null) // seems like onSensorChanged can be called after unregisterListener for unhandled events
+        if (proximity == null) // seems like onSensorChanged can be called after unregisterListener for unhandled events
             return;
         float distance = event.values[0];
         if (distance <= 0) { // always 0 or 5 on my device (cm)
@@ -159,25 +164,5 @@ public class ProximitySensor implements SensorEventListener {
     }
 
     public void onFar() {
-    }
-
-    void showSystemUI(Window w) {
-        if (Build.VERSION.SDK_INT >= 11) {
-            w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
-    }
-
-    void hideSystemUI(Window w) {
-        // Set the IMMERSIVE flag.
-        // Set the content to appear under the system bars so that the content
-        // doesn't resize when the system bars hide and show.
-        if (Build.VERSION.SDK_INT >= 11) {
-            w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
     }
 }
