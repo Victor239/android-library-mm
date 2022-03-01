92d7b6cc4c116769d352d3511d7b69b3816a82e6
Alexey Kuznetsov <axet@me.com>
2022-03-01 06:04:52 +0300

    cleanups

--- src/main/java/com/github/axet/androidlibrary/sound/AudioTrack.java
+++ src/main/java/com/github/axet/androidlibrary/sound/AudioTrack.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 
 public class AudioTrack extends android.media.AudioTrack {
     public static final int SHORT_SIZE = Short.SIZE / Byte.SIZE;
+    public static final int FLOAT_SIZE = Float.SIZE / Byte.SIZE;
 
     public long playStart = 0;
     public int len; // len in frames (stereo frames = len * 2)
@@ -252,17 +253,22 @@ public class AudioTrack extends android.media.AudioTrack {
         }
 
         public void write(int pos, short s, int cn) {
-            switch (cn) {
-                case 1:
-                    write(pos, s);
-                    break;
-                case 2:
-                    write(pos, s, s);
-                    break;
-                default:
-                    for (int i = 0; i < cn; i++)
-                        buffer.shorts[pos + i] = s;
-            }
+            for (int i = 0; i < cn; i++)
+                buffer.shorts[pos + i] = s;
+        }
+
+        public void write(int pos, float f) {
+            buffer.floats[pos] = f;
+        }
+
+        public void write(int pos, float f, float f2) {
+            buffer.floats[pos] = f;
+            buffer.floats[pos + 1] = f2;
+        }
+
+        public void write(int pos, float s, int cn) {
+            for (int i = 0; i < cn; i++)
+                buffer.floats[pos + i] = s;
         }
 
         public void reset() {
@@ -270,7 +276,14 @@ public class AudioTrack extends android.media.AudioTrack {
         }
 
         public int getBytesLen() {
-            return buffer.capacity * SHORT_SIZE;
+            switch (a) {
+                case AudioFormat.ENCODING_PCM_16BIT:
+                    return buffer.capacity * SHORT_SIZE;
+                case AudioFormat.ENCODING_PCM_FLOAT:
+                    return buffer.capacity * FLOAT_SIZE;
+                default:
+                    throw new RuntimeException("unknown format");
+            }
         }
 
         public int getBytesMin() {
