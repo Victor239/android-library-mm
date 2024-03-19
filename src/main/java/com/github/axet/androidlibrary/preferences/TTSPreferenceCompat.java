c4cc03695ef06b0187e731b0a7542d06929cc97a
Alexey Kuznetsov <axet@me.com>
2024-03-19 08:00:58 +0300

    fix vertial spannable image

--- src/main/java/com/github/axet/androidlibrary/preferences/TTSPreferenceCompat.java
+++ src/main/java/com/github/axet/androidlibrary/preferences/TTSPreferenceCompat.java
@@ -4,6 +4,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.LocaleList;
@@ -114,6 +117,7 @@ public class TTSPreferenceCompat extends ListPreference {
 
     public static CharSequence getImageText(final Context context, int res, final int tint) {
         Drawable d = context.getResources().getDrawable(res);
+        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
         d = DrawableCompat.wrap(d);
         DrawableCompat.setTint(d, ThemeUtils.getThemeColor(context, tint));
         return getImageText(d);
@@ -121,10 +125,48 @@ public class TTSPreferenceCompat extends ListPreference {
 
     public static CharSequence getImageText(Drawable d) {
         SpannableStringBuilder t = new SpannableStringBuilder();
+        int start = t.length();
         t.append("!");
-        ImageSpan img = new ImageSpan(d);
-        t.setSpan(img, t.length() - 1, t.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int end = t.length();
+        VerticalImageSpan img = new VerticalImageSpan(d);
+        t.setSpan(img, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         return t;
+    }
+
+    public static class VerticalImageSpan extends ImageSpan {
+        public VerticalImageSpan(Drawable drawable) {
+            super(drawable);
+        }
+
+        @Override
+        public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fontMetricsInt) {
+            Drawable drawable = getDrawable();
+            Rect rect = drawable.getBounds();
+            if (fontMetricsInt != null) {
+                Paint.FontMetricsInt fmPaint = paint.getFontMetricsInt();
+                int fontHeight = fmPaint.descent - fmPaint.ascent;
+                int drHeight = rect.bottom - rect.top;
+                int centerY = fmPaint.ascent + fontHeight / 2;
+                fontMetricsInt.ascent = centerY - drHeight / 2;
+                fontMetricsInt.top = fontMetricsInt.ascent;
+                fontMetricsInt.bottom = centerY + drHeight / 2;
+                fontMetricsInt.descent = fontMetricsInt.bottom;
+            }
+            return rect.right;
+        }
+
+        @Override
+        public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
+            Drawable drawable = getDrawable();
+            canvas.save();
+            Paint.FontMetricsInt fmPaint = paint.getFontMetricsInt();
+            int fontHeight = fmPaint.descent - fmPaint.ascent;
+            int centerY = y + fmPaint.descent - fontHeight / 2;
+            int transY = centerY - (drawable.getBounds().bottom - drawable.getBounds().top) / 2;
+            canvas.translate(x, transY);
+            drawable.draw(canvas);
+            canvas.restore();
+        }
     }
 
     public TTSPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr) {
